e5a338daa0b89e86bde3fe814e57af700a978aab
Alexandre <alexandre.g.serra@outlook.com>
2017-10-09 22:54:55 -0300

    String no web config

--- LaboratorioAlexandre/Repository/DAO/Conexao.cs
+++ LaboratorioAlexandre/Repository/DAO/Conexao.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 using System.Data;
+using System.Configuration;
 
 namespace LaboratorioAlexandre.Repository.DAO
 {
@@ -12,7 +13,7 @@ namespace LaboratorioAlexandre.Repository.DAO
         private SqlConnection Conectar()
         {
             //Para poder ter acesso ao BD
-            string stringConexao = "Data Source = LAB04-08 ; Initial Catalog = BD_Laboratorio6; User Id = sa ; Password = uniron$2017";
+            string stringConexao = ConfigurationManager.ConnectionStrings["ConexaoAtendimentoHospitalar"].ConnectionString;
             //Conectar o BD
             SqlConnection conexao = new SqlConnection(stringConexao);
             //Abrir o BD
