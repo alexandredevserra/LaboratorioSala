0b8c70545ea18755104c8e1141bb2eeb636c2780
alexandre@ <alexandre.p.serra@>
2017-10-16 23:06:25 -0200

    View listar cidade

---
+++ LaboratorioAlexandre/Controllers/CidadeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaboratorioAlexandre.Controllers
+{
+    public class CidadeController : Controller
+    {
+        // GET: Cidade
+        public ActionResult Listar()
+        {
+            return View();
+        }
+    }
+}
